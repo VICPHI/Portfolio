a809bb2957a596c96d39bf529243dd2fb525d015
VICPHI <victor@mosaique.dk>
2025-01-13 22:27:41 +0100

    Add files via upload

---
+++ header.php
@@ -0,0 +1,14 @@
+<header>
+    <!-- Logo og titel -->
+    <h1>Min Portfolio</h1>
+
+    <!-- Navigationen -->
+    <nav>
+        <ul>
+            <li><a href="index.php">Hjem</a></li>
+            <li><a href="om-mig.php">Om mig</a></li>
+            <li><a href="projekter.php">Projekter</a></li>
+            <li><a href="kontakt.php">Kontakt</a></li>
+        </ul>
+    </nav>
+</header>
